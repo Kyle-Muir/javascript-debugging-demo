2266429233fb50cd3337afecdf8a1a0679fc2d80
Kyle-Muir <kyle.matthew.muir@gmail.com>
2015-05-23 17:38:50 +1200

    Added basic webapi with echo service

---
+++ webapi/JavaScriptDebuggingDemoWebApi/Controllers/EchoController.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Web.Http;
+
+namespace JavaScriptDebuggingDemoWebApi.Controllers
+{
+    public class EchoController : ApiController
+    {
+        // GET api/echo
+        public string Get()
+        {
+            return "Echo";
+        }
+
+        // GET api/echo/5
+        public string Get(int idToEcho)
+        {
+            return idToEcho.ToString(CultureInfo.CurrentCulture);
+        }
+    }
+}
---
+++ webapi/JavaScriptDebuggingDemoWebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JavaScriptDebuggingDemoWebApi.WebApiApplication" Language="C#" %>
