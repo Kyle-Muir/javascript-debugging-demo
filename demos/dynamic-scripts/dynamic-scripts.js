79930c0f298486c69f460c911be40f4522e7d225
Kyle-Muir <kyle.matthew.muir@gmail.com>
2015-05-28 10:51:10 +1200

    Finishing off dynamics scripts example

--- demos/dynamic-scripts/dynamic-scripts.js
+++ demos/dynamic-scripts/dynamic-scripts.js
@@ -1,6 +1,12 @@
 function executeDemo() {
-    $.get('http://localhost:53173/Content/dynamic-script-with-source-url.js').success(function(resp){eval(resp); console.log(resp)})
-    $.get('http://localhost:53173/Content/dynamic-script-no-source-url.js').success(function(resp){eval(resp); console.log(resp)})
+    $.get('http://localhost:53173/Content/dynamic-script-with-source-url.js').success(function(resp){
+    	//NOTE: Never do this other than for demos. Eval is generally pretty evil.
+    	eval(resp);
+    });
+    $.get('http://localhost:53173/Content/dynamic-script-no-source-url.js').success(function(resp){
+    	//NOTE: Never do this other than for demos. Eval is generally pretty evil.
+    	eval(resp);
+    });
 }
 
 $(function() {
